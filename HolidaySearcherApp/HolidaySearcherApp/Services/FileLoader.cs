7022ae32e7ba10572e16ef230dfb4310fa8e59c4
Hayley96 <h.jonas@outlook.com>
2022-07-24 01:15:06 +0100

    Added FileLoader class containing two methods: 1) Path - determines if file exists in given path, if so return path else empty string 2) Load - returns the data from file Added tests for methods - currently passing

---
+++ HolidaySearcherApp/HolidaySearcherApp/Services/FileLoader.cs
@@ -0,0 +1,20 @@
+﻿namespace HolidaySearcherApp.Services
+{
+    public static class FileLoader
+    {
+        public static string PathToFile { get; private set; } = string.Empty;
+        public static string Path(string filename)
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+
+            PathToFile = (currentDir.ToLower().Contains(@"\bin\debug") || currentDir.ToLower().Contains(@"\bin\release")) ?
+                 Directory.GetParent(currentDir)!.Parent!.Parent!.FullName + $"\\Data\\{filename}".ToString() :
+                 $"{currentDir}\\Data\\{filename}".ToString();
+
+            return File.Exists(PathToFile) ? PathToFile : string.Empty;
+        }
+
+        public static string Load(string file) =>
+            File.ReadAllText(file);
+    }
+}
---
+++ HolidaySearcherAppTests/ServicesTests/FileLoaderTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using HolidaySearcherApp.Services;
+
+namespace HolidaySearcherAppTests.ServicesTests
+{
+    public class FileLoaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Path_Returns_Path_To_Data_File()
+        {
+            //Arrange
+            var expectedresult = "Flights.json";
+
+            //Act
+            var result = FileLoader.Path("Flights.json");
+
+            //Assert
+            result.Should().Contain(expectedresult);
+        }
+
+        [Test]
+        public void Path_Returns_Empty_String_If_File_Not_Found()
+        {
+            //Arrange
+            var expectedresult = string.Empty;
+
+            //Act
+            var result = FileLoader.Path("Flhts.json");
+
+            //Assert
+            result.Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void Load_Returns_String_Of_Data_From_File()
+        {
+            //Arrange
+            var expectedresult1 = "Oceanic Airlines";
+            var expectedresult2 = "Fresh Airways";
+            var file = FileLoader.Path("Flights.json");
+
+            //Act
+            var result = FileLoader.Load(file);
+
+            //Assert
+            result.Should().Contain(expectedresult1);
+            result.Should().Contain(expectedresult2);
+        }
+    }
+}
